6b4b1bca37061355ac3ce53e02f94a7ca7e5dfe9
Mohamed Fawzan <fawzanm@gmail.com>
2020-04-04 12:40:37 +0530

    added block package

---
+++ lib/Repositories/user_repository.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:chopper/chopper.dart';
+import 'package:j3enterprise/api_clients/api_client.dart';
+import 'package:j3enterprise/services/rest_api_service.dart';
+import 'package:meta/meta.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserRepository {
+  final api = ApiClient.chopper.getService<RestApiService>();
+
+  Future<Response> authenticate({
+    @required String username,
+    @required String password,
+  }) async {
+    return await api.login({
+      "rememberClient": true,
+      "userNameOrEmailAddress": username,
+      "password": password
+    });
+  }
+
+  Future<Response> getUser({@required int userID}) async {
+    return await api.getUser(userID);
+  }
+
+  Future<void> deleteToken() async {
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+    return _prefs.remove('access_token');
+  }
+
+  Future<void> persistToken(String token) async {
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+    _prefs.setString("access_token", token);
+    return;
+  }
+
+  Future<bool> hasToken() async {
+    SharedPreferences _prefs = await SharedPreferences.getInstance();
+    return _prefs.containsKey('access_token');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:j3enterprise/screens/login/login_screen.dart';
 import 'package:j3enterprise/shared/utils/routes.dart';
 import 'package:j3enterprise/shared/widgets/custom_drawer.dart';
+import 'package:j3enterprise/splash/splash_page.dart';
 import 'package:logging/logging.dart';
 
-void main() {
-  _setupLogging();
-  runApp(MyApp());
-}
+import 'Repositories/user_repository.dart';
+import 'authentication/authentication_bloc.dart';
+import 'authentication/authentication_event.dart';
+import 'authentication/authentication_state.dart';
+import 'common/loading_indicator.dart';
+import 'home/home_page.dart';
+import 'login/login_page.dart';
 
+//
+//void main() {
+//  runApp(MyApp());
+//}
+//
 void _setupLogging() {
   Logger.root.level = Level.ALL;
   Logger.root.onRecord.listen((rec) {
@@ -16,20 +26,62 @@ void _setupLogging() {
   });
 }
 
-class MyApp extends StatelessWidget {
+class SimpleBlocDelegate extends BlocDelegate {
+  @override
+  void onEvent(Bloc bloc, Object event) {
+    super.onEvent(bloc, event);
+    print(event);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print(transition);
+  }
+
+  @override
+  void onError(Bloc bloc, Object error, StackTrace stacktrace) {
+    super.onError(bloc, error, stacktrace);
+    print(error);
+  }
+}
+
+void main() {
+  _setupLogging();
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+  final userRepository = UserRepository();
+  runApp(
+    BlocProvider<AuthenticationBloc>(
+      create: (context) {
+        return AuthenticationBloc(userRepository: userRepository)
+          ..add(AppStarted());
+      },
+      child: App(userRepository: userRepository),
+    ),
+  );
+}
+
+class App extends StatelessWidget {
+  final UserRepository userRepository;
+
+  App({Key key, @required this.userRepository}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('J3 Enterprise Solutions'),
-          backgroundColor: Colors.blue,
-        ),
-        body: SafeArea(
-          child: LoginScreen(),
-        ),
-        resizeToAvoidBottomInset: false,
-        drawer: CustomDrawer(),
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: (context, state) {
+          if (state is AuthenticationAuthenticated) {
+            return HomePage();
+          }
+          if (state is AuthenticationUnauthenticated) {
+            return LoginPage(userRepository: userRepository);
+          }
+          if (state is AuthenticationLoading) {
+            return LoadingIndicator();
+          }
+          return SplashPage();
+        },
       ),
       theme: ThemeData(
         primarySwatch: Colors.blue,
