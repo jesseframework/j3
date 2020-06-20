0c4dad9322b28e392a25b76bf2a49bce164b91f5
jesseframework <jgray@cpxzgroup.com>
2020-06-20 05:58:10 -0500

    Background jOb

--- lib/src/resources/shared/function/schedule_background_jobs.dart
+++ lib/src/resources/shared/function/schedule_background_jobs.dart
@@ -1,6 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
+import 'package:j3enterprise/src/database/crud/backgroundjob/backgroundjob_schedule_crud.dart';
+import 'package:j3enterprise/src/database/moor_database.dart';
+import 'package:j3enterprise/src/resources/repositories/applogger_repositiry.dart';
+import 'package:logging/logging.dart';
 
 ///This code will allow user to set the frequency of code run in background job.
 ///Once enable dart timer will execute API call and updat date base base on timer.
@@ -13,7 +17,8 @@ class TimerData {
 }
 
 class Scheduleler {
-  //Timer timer;
+  static final _log = Logger('Scheduleler');
+
   List<TimerData> timers = [];
 
   void cancel(String jobName) {
@@ -47,8 +52,47 @@ class Scheduleler {
 
     timers.add(new TimerData(
         name: jobName, timer: Timer.periodic(duration, callback)));
-    
+  }
+
+  Future<void> runNowJobs(
+      String setFrequency, String jobName, Function(Timer) callback) async {
+    var duration = _getFromString(setFrequency);
+    if (duration == null) return;
+
+    timers.add(
+        new TimerData(name: jobName, timer: Timer(duration, () => callback)));
   }
 }
 
+Future<void> syncClickleScheduler() async {
+  var db;
+  db = AppDatabase();
+  BackgroundJobScheduleDao backgroundJobScheduleDao =
+      new BackgroundJobScheduleDao(db);
+  AppLoggerRepository appLoggerRepository = new AppLoggerRepository();
+  Scheduleler scheduleler = new Scheduleler();
+  var jobData = await backgroundJobScheduleDao.getAllJobs();
+  for (var eachJob in jobData) {
+    scheduleler.runNowJobs(
+        eachJob.syncFrequency,
+        eachJob.jobName,
+        (Timer timer) =>
+            appLoggerRepository.putAppLogOnServer(eachJob.jobName));
 
+            
+  }
+}
+
+Future<void> syncClickCancel() async {
+  var db;
+  db = AppDatabase();
+  BackgroundJobScheduleDao backgroundJobScheduleDao =
+      new BackgroundJobScheduleDao(db);
+
+  Scheduleler scheduleler = new Scheduleler();
+  var jobData = await backgroundJobScheduleDao.getAllJobs();
+  for (var eachJob in jobData) {
+    
+    scheduleler.cancel(eachJob.jobName);
+  }
+}
