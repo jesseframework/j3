c47baf6d7334ea5efc3056880b5380608eebc608
Jesse Framework <61049847+jesseframework@users.noreply.github.com>
2020-07-02 16:22:55 -0500

    Update Application.kt

--- android/app/src/main/kotlin/com/j3enterprisecloud/j3enterprise/Application.kt
+++ android/app/src/main/kotlin/com/j3enterprisecloud/j3enterprise/Application.kt
@@ -1,5 +1,3 @@
-package com.j3enterprisecloud.j3enterprise
-
 import com.transistorsoft.flutter.backgroundfetch.BackgroundFetchPlugin
 import io.flutter.app.FlutterApplication
 import io.flutter.embedding.engine.FlutterEngine
@@ -8,16 +6,17 @@ import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
 import io.flutter.plugins.GeneratedPluginRegistrant
 import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
 
-class Application : FlutterApplication(), PluginRegistrantCallback {
 
+class Application:FlutterApplication(), PluginRegistrantCallback {
     override fun onCreate() {
         super.onCreate()
         FlutterFirebaseMessagingService.setPluginRegistrant(this)
-    BackgroundFetchPlugin.setPluginRegistrant(this);
+        BackgroundFetchPlugin.setPluginRegistrant(this);
     }
 
-    override fun registerWith(registry: PluginRegistry?) {
+
+        override fun registerWith(registry: PluginRegistry?) {
         io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin.registerWith(registry?.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
-    GeneratedPluginRegistrant.registerWith((registry as FlutterEngine?)!!)
+        GeneratedPluginRegistrant.registerWith(registry);
     }
 }
