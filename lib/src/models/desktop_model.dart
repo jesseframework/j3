3e43958fc964e82db074713f78049cebd498a5a2
CPXZ\ibashir <ibashir@cpxzgroup.com>
2020-07-16 19:04:40 -0400

    JG #JF- 83 Rebased

--- lib/src/models/desktop_model.dart
+++ lib/src/models/desktop_model.dart
@@ -3,7 +3,9 @@ import 'package:moor/moor.dart';
 class Desktop extends Table {
   IntColumn get id => integer().autoIncrement()();
   TextColumn get iconName => text()();
-  TextColumn get iconImages => text()();
+  TextColumn get iconCode => text()();
+  TextColumn get iconColour => text()();
+  TextColumn get iconFamily => text()();
   TextColumn get navigationRoute => text()();
   TextColumn get iconGroup => text()();
   BoolColumn get isFavorit => boolean().withDefault(Constant(false))();
